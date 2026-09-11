e2c35c2cb46994f703226ff84d1e07c59048fdbc
topkidx <iliv0394@students.vsu.edu>
2026-09-11 11:15:27 -0400

    Complete Lab 1 counter app

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,30 +2,23 @@ package com.example.helloworld
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
 
         val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
 
         button.setOnClickListener {
-            Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
         }
     }
 }
